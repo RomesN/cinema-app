0bf323280f3018574a7b5c71ce5adb2be4708454
Roman <romannemeth1@gmail.com>
2023-09-13 10:40:45 +0200

    updated readme, added header component and styles

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootReducers from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
 
 export const store = configureStore({
   reducer: rootReducers,
   devTools: true,
-  middleware: composeWithDevTools(applyMiddleware(...middleware)),
+  middleware: [thunk],
   preloadedState: initialState
 });
