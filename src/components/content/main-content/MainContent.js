bb26c72d545e5fdd1c8af57a6f496f3cbab64b1b
Roman <romannemeth1@gmail.com>
2023-09-23 15:15:51 +0200

    grid-component

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -3,19 +3,84 @@ import './MainContent.scss';
 
 import SlideShow from '../slide-show/SlideShow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 export const MainContent = () => {
   const images = [
     {
-      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 5.5
     },
     {
-      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
     },
     {
-      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 5.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 5.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
     }
   ];
+
   const [currentPage, setCurrentPage] = useState(1);
 
   const paginate = (type) => {
@@ -35,6 +100,7 @@ export const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
+      <Grid images={images} />
     </div>
   );
 };
---
+++ src/components/content/rating/Rating.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import './Rating.scss';
+
+const Rating = ({ rating, totalStars, className }) => {
+  const [numberOfStars, setNumberOfStars] = useState();
+  const ratingRef = useRef();
+
+  useEffect(() => {
+    setNumberOfStars([...Array(totalStars).keys()].map((i) => i + 1));
+    let percentage;
+    if (rating <= 5) {
+      percentage = (rating / 5) * 100;
+    } else {
+      percentage = (rating / 10) * 100;
+    }
+    const startPercentage = `${Math.floor(percentage)}%`;
+    ratingRef.current.style.width = startPercentage;
+  }, [rating, totalStars]);
+
+  return (
+    <div className="star-rating">
+      <div className={`back-stars ${className}`}>
+        {numberOfStars && numberOfStars.map((i) => <i className="fa fa-star" aria-hidden="true" key={i}></i>)}
+
+        <div className={`front-stars ${className}`} ref={ratingRef}>
+          {numberOfStars && numberOfStars.map((i) => <i className="fa fa-star" aria-hidden="true" key={i}></i>)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+  totalStars: PropTypes.number.isRequired,
+  className: PropTypes.string
+};
+
+export default Rating;
